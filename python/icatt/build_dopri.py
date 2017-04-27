0fb1012b3639a6d6c53d80cd00b43b72a67b8022
Helge Eichhorn <git@helgeeichhorn.de>
2017-04-27 08:37:59 +0200

    Cleanup

--- python/icatt/build_dopri.py
+++ python/icatt/build_dopri.py
@@ -7,10 +7,6 @@ includepath = os.path.abspath(os.path.join("..","..","lib","dopri"))
 ffi = FFI()
 
 ffi.cdef("""
-extern "Python" void gravity(int *, double *, double *, double *, double *, int *);
-
-void c_gravity(int *n, double *x, double *y, double *f, double *rpar, int *ipar);
-
 extern "Python" void solout(int *, double *, double *, double *, int *, double *,
                       int *, int *, double *, int *, int *, double *);
 
@@ -34,28 +30,6 @@ void c_dop853(
     int *ipar,
     int *idid);
 """)
-
-ffi.set_source("_dopri",
-    """
-    #include <math.h>
-    #include "dopri.h"
-
-    void c_gravity(int *n, double *x, double *y, double *f, double *rpar, int *ipar) {
-        double r = sqrt(y[0] * y[0] + y[1] * y[1] + y[2] * y[2]);
-        double r3 = r*r*r;
-        f[0] = y[3];
-        f[1] = y[4];
-        f[2] = y[5];
-        f[3] = -rpar[0] * y[0] / r3;
-        f[4] = -rpar[0] * y[1] / r3;
-        f[5] = -rpar[0] * y[2] / r3;
-    }
-    """,
-    libraries=["dopri"],
-    include_dirs=[includepath],
-    library_dirs=[libpath],
-    extra_link_args=['-Wl,-rpath,{libpath}'.format(libpath=libpath)],
-)
 
 if __name__ == "__main__":
     ffi.compile()
--- python/icatt/dopri.py
+++ python/icatt/dopri.py
@@ -4,7 +4,7 @@ from icatt._dopri import ffi, lib
 import icatt.kepler as kepler
 import icatt.elements as elements
 import time
-from numba import cfunc, types, carray
+from numba import cfunc, types, farray
 
 def benchmark(times):
     y = np.array([8.59072560e+02, -4.13720368e+03, 5.29556871e+03, 7.37289205e+00, 2.08223573e+00, 4.39999794e-01])
@@ -14,16 +14,14 @@ def benchmark(times):
     rpar = np.array([mu])
     el = elements.elements(y[0:3], y[3:], mu)
     tp = kepler.period(el[0], mu)
-    # dopri(lib.gravity, x, y, tp, rpar)
-    dopri(ffi.addressof(lib, "c_gravity"), x, y, tp, rpar)
+    dopri(numba_gravity.cffi, x, y, tp, rpar)
 
     best = np.inf
     worst = -np.inf
     total = 0.0
     for _ in range(times):
         t0 = time.clock()
-        # dopri(lib.gravity, x, y, tp, rpar)
-        dopri(ffi.addressof(lib, "c_gravity"), x, y, tp, rpar)
+        dopri(numba_gravity.cffi, x, y, tp, rpar)
         t1 = time.clock()
         x = 0.0
         y = y0.copy()
@@ -35,16 +33,31 @@ def benchmark(times):
         total += current
     print("[",total/times,",",best,",",worst,"]")
 
-@ffi.def_extern()
-def gravity(n, x, y, f, rpar, ipar):
+c_sig = types.void(
+    types.CPointer(types.intc),
+    types.CPointer(types.double),
+    types.CPointer(types.double),
+    types.CPointer(types.double),
+    types.CPointer(types.double),
+    types.CPointer(types.intc),
+)
+
+@cfunc(c_sig)
+def numba_gravity(_n, _x, _y, _f, _rpar, _ipar):
+    n = _n[0]
+    x = _x[0]
+    y = farray(_y, 6)
+    f = farray(_f, 6)
+    mu = _rpar[0]
+
     r = np.sqrt(y[0]*y[0]+y[1]*y[1]+y[2]*y[2])
     r3 = r*r*r
     f[0] = y[3]
     f[1] = y[4]
     f[2] = y[5]
-    f[3] = -rpar[0]*y[0]/r3
-    f[4] = -rpar[0]*y[1]/r3
-    f[5] = -rpar[0]*y[2]/r3
+    f[3] = -mu*y[0]/r3
+    f[4] = -mu*y[1]/r3
+    f[5] = -mu*y[2]/r3
 
 @ffi.def_extern()
 def solout(nr, xold, x, y, n, con, icomp, nd, rpar, ipar, irtrn, xout):
